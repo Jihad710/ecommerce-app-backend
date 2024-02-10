fc1b69b2964b0c43416c5672532108be3fb96f71
jihad710 <dvfaruq@insaaftechsolutions.com>
2024-02-10 16:11:55 +0600

    done reset password

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -5,6 +5,7 @@ const validateMongoDbId = require("../utils/validateMongodbId");
 const { generateRefreshToken } = require("../config/refreshtoken");
 const jwt = require("jsonwebtoken");
 const crypto = require("crypto");
+const sendEmail = require("./emailCtrl");
 
 // create a user
 const createUser = asyncHandler(async (req, res) => {
@@ -212,6 +213,29 @@ const updatePassword = asyncHandler(async (req, res) => {
   }
 });
 
+
+const forgotPasswordToken = asyncHandler(async (req, res) => {
+  const { email } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) throw new Error("User not found with this email");
+  try {
+    const token = await user.createPasswordResetToken();
+    await user.save();
+    const resetURL = `Hi, Please follow this link to reset Your Password. This link is valid till 10 minutes from now. <a href='http://localhost:4001/api/user/reset-password/${token}'>Click Here</>`;
+    const data = {
+      to: email,
+      text: "Hey User",
+      subject: "Forgot Password Link",
+      htm: resetURL,
+    };
+    sendEmail(data);
+    res.json(token);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+
 module.exports = {
   createUser,
   loginUserCtrl,
@@ -224,4 +248,5 @@ module.exports = {
   handleRefreshToken,
   logout,
   updatePassword,
+  forgotPasswordToken,
 };
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -12,10 +12,12 @@ const {
   handleRefreshToken,
   logout,
   updatePassword,
+  forgotPasswordToken
 } = require("../controller/userCtrl");
 const { authMiddleware, isAdmin } = require("../middlewares/authMiddleware");
 
 router.post("/register", createUser);
+router.post("/forgot-password-token", forgotPasswordToken);
 router.put("/password",authMiddleware, updatePassword);
 router.post("/login", loginUserCtrl);
 router.get("/all-users", getallUser);
